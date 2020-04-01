b44938294574300eafbf270be97b76efdef21903
robyjacob <robyjacob1998@gmail.com>
2020-04-02 02:01:20 +0530

    delete button added

--- assignments/project0/script.js
+++ assignments/project0/script.js
@@ -1,14 +1,50 @@
 const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
+	TODO_ITEM: 'todo-container',
+	TODO_CHECKBOX: 'todo-checkbox',
+	TODO_TEXT: 'todo-text',
+	TODO_DELETE: 'todo-delete',
 }
 
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+let itemCount = 0;
+let uncheckedItemCount = 0;
+
+function updateItemCount() {
+	itemCountSpan.innerHTML = itemCount;
+}
+
+function updateUncheckedItemCount() {
+	uncheckedCountSpan.innerHTML = uncheckedItemCount;
+}
 
 function newTodo() {
-  alert('New TODO button clicked!')
+	let input = prompt("Enter TODO");
+
+	let item = document.createElement("li");
+	item.className = classNames.TODO_ITEM;
+
+	let itemCheckeBox = document.createElement("input");
+	itemCheckeBox.setAttribute("type", "checkbox");
+	itemCheckeBox.className = classNames.TODO_CHECKBOX;
+
+	let text = document.createElement("span");
+	text.className = classNames.TODO_TEXT;
+	text.innerHTML = input;
+	
+	let itemDelete = document.createElement("button");
+	itemDelete.innerText = "Delete";
+	itemDelete.className = classNames.TODO_DELETE;
+	itemDelete.addEventListener("click", function() { list.removeChild(this.parentNode); itemCount--; updateItemCount(); });
+	
+	item.appendChild(text);
+	item.appendChild(itemCheckeBox);
+	item.appendChild(itemDelete);
+
+	list.appendChild(item);
+
+	itemCount++;
+	updateItemCount();
+	updateUncheckedItemCount();
 }
